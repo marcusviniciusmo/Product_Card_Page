061af6602baf0c22b47c41d44bdbc2110144e9b9
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-01-15 21:57:31 +0000

    Product component styling

--- frontend/src/components/Product/index.tsx
+++ frontend/src/components/Product/index.tsx
@@ -1,11 +1,44 @@
+import { useState } from 'react';
+import ImageProduct from '../../assets/product.svg';
+import Vector from '../../assets/vector.svg';
 import { Card } from "components/Card";
+import * as Styles from "./styles";
 
 export function Product() {
-  return (
-    <>
-      <h1>PRODUCT Component</h1>
+  const [showCard, setShowCard] = useState<boolean>(false);
 
-      <Card />
-    </>
+  const toggleShowCard = () => setShowCard(!showCard);
+
+  return (
+    <Styles.Container>
+      <Styles.ImageContainer>
+        {
+          showCard
+            ? <Card />
+            : <Styles.Image src={ImageProduct} />
+        }
+        <Styles.Vector src={Vector} onClick={toggleShowCard} />
+      </Styles.ImageContainer>
+
+      <Styles.InfoContainer>
+        <Styles.InfoContent>
+          <Styles.Code>Código: 42004</Styles.Code>
+
+          <Styles.HeadingProductName>
+            <Styles.ProductName>
+              Sofá Margot II - Rosé
+            </Styles.ProductName>
+          </Styles.HeadingProductName>
+
+          <Styles.HeadingProductPrice>
+            <Styles.ProductPrice>R$ 4.000</Styles.ProductPrice>
+          </Styles.HeadingProductPrice>
+        </Styles.InfoContent>
+
+        <Styles.Button>
+          <Styles.ButtonText>Adicionar à cesta</Styles.ButtonText>
+        </Styles.Button>
+      </Styles.InfoContainer>
+    </Styles.Container>
   );
 };
---
+++ frontend/src/components/Product/styles.tsx
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border: 0.1rem solid var(--color-primary);
+  border-radius: 1rem;
+  width: 87.5rem;
+  height: 49.8rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1.3rem;
+`;
+
+export const ImageContainer = styled.div`
+  width: 44.9rem;
+  height: 22.2rem;
+  position: relative;
+`;
+
+export const Image = styled.img``;
+
+export const Vector = styled.img`
+  position: absolute;
+  top: 3.8%;
+  left: 81.51%;
+  cursor: pointer;
+`;
+
+export const InfoContainer = styled.div`
+  width: 28.1rem;
+  height: 49.8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  align-self: stretch;
+  gap: 2rem;
+`;
+
+export const InfoContent = styled.div`
+  color: var(--color-primary);
+  width: 28.1rem;
+  height: 8.2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.2rem;
+`;
+
+export const Code = styled.span`
+  font-family: 'Lato';
+  font-size: 1rem;
+  font-weight: 300;
+`;
+
+export const HeadingProductName = styled.div`
+  width: 27.4rem;
+  height: 3.2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+`;
+
+export const ProductName = styled.span`
+  font-family: 'Crimson Pro';
+  font-size: 3.2rem;
+  font-weight: 600;
+`;
+
+export const HeadingProductPrice = styled.div`
+  width: 28.1rem;
+  height: 1.6rem;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  opacity: 0.7;
+`;
+
+export const ProductPrice = styled.span`
+  font-family: 'Lato';
+  font-size: 1.6rem;
+  font-weight: 400;
+`;
+
+export const Button = styled.button`
+  border-radius: 10rem;
+  border: 0.05rem solid var(--color-primary);
+  width: 14.8rem;
+  height: 3.2rem;
+  padding: 0.8rem 1.6rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+  box-sizing: border-box;
+
+  &:hover {
+    background: var(--color-primary);
+    color: #FFFFFF;
+  }
+`;
+
+export const ButtonText = styled.span`
+  font-family: 'Lato';
+  font-size: 1.15rem;
+  font-weight: 400;
+  line-height: 1.6rem;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
